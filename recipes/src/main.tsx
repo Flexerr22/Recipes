f97e26374ec9b50a7d54103dc5c4f4a368769482
Flexerr22 <ilia2005951@gmail.com>
2024-08-25 01:26:09 +0300

    Создал layout добавил роутеры

--- recipes/src/main.tsx
+++ recipes/src/main.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu.tsx';
+import { Error } from './pages/Error/Error.tsx';
+import { Favourites } from './pages/Favourites/Favourites.tsx';
+import { Profile } from './pages/Profile/Profile.tsx';
+import { Layout } from './layout/Menu/Layout.tsx';
+import { AddRecipe } from './pages/AddRecipe/AddRecipe.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Menu />
+      },
+      {
+        path: '/favourites', 
+        element: <Favourites />
+      },
+      {
+        path: '/profile', 
+        element: <Profile />
+      },
+      {
+        path: '/addrecipe', 
+        element: <AddRecipe />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <Error />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
