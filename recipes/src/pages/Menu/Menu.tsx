77c7fb757c7b6a7fcc91f6be8acca581af9831bc
Flexerr22 <ilia2005951@gmail.com>
2024-09-03 02:21:22 +0300

    Сделал рабочие сортировки по сайту

--- recipes/src/pages/Menu/Menu.tsx
+++ recipes/src/pages/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Headlink from "../../components/Headlink/Headlink";
 import { PREFIX } from "../../components/helpers/API";
 import { Product } from "../../components/interfaces/Product.interface";
@@ -11,39 +11,64 @@ import Search from "../../components/Search/Search";
 export function Menu(){
     const[products,setProducts] = useState<Product[]>([])
     const [searchItem, setSearchItem] = useState<string>('')
+    const [sortValue, setSortValue] = useState<string>('')
+    const [sortValueNew, setSortValueNew] = useState<string>('')
 
-    useEffect(() => {
-        getProduct(searchItem)
-    },[searchItem])
-
-
-    const getProduct = async (name?: string) => {
+    const getProduct = useCallback( async (name?: string) => {
         try{
             const {data} = await axios.get<Product[]>(`${PREFIX}`, {
                 params: {name}
             })
-            setProducts(data)
+            const sortedData = filterProducts(data, sortValue, sortValueNew);
+            setProducts(sortedData);
         } catch(e){
             console.error(e)
             return
         }
         
-    }
+    },[sortValue, sortValueNew])
+
+    useEffect(() => {
+        getProduct(searchItem)
+    },[searchItem, getProduct])
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchItem(event.target.value);
     };
+
+    const sortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSortValue(event.target.value)
+    }
+
+    const sortChangeNew = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSortValueNew(event.target.value)
+    }
+
+    const filterProducts = (products: Product[], sortValue: string, sortValueNew: string): Product[] => {
+        if(sortValue){
+            return products.filter(product => product.price.toString() === sortValue);
+
+        }
+        else if(sortValueNew){
+            return products.filter(product => product.ingredients.length.toString() === sortValueNew);
+        }
+        return products
+    }
     
     return <div className={styles['main']}>
     <div className={styles['sort']}>
             <div className={styles['select']}>
-                <select name="" id="">
-                    <option value="290">290</option>
-                    <option value="280">280</option>
-                    <option value="300">300</option>
+                <select onChange={sortChange} name="" id="">
+                    <option value="">Сортировка по времени</option>
+                    <option value="290">Время: 290</option>
+                    <option value="280">Время: 280</option>
+                    <option value="300">Время: 300</option>
+                    <option value="320">Время: 320</option>
                 </select>
-                <select name="" id="">
-                    <option value="4">Кол-во ингридиентов</option>
+                <select onChange={sortChangeNew} name="" id="">
+                    <option value="">Кол-во ингридиентов</option>
+                    <option value="4">Кол-во ингридиентов: 4</option>
+                    <option value="3">Кол-во ингридиентов: 3</option>
                 </select>
             </div>
             <Search onChange={handleChange} placeholder="Введите блюдо"/>
